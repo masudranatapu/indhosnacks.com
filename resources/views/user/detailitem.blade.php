24390c1b4e3a49e6dd3a7ff20026919e765cd881
Masud Rana Tapu <91066147+masudranatapu@users.noreply.github.com>
2023-01-19 17:40:54 +0600

    done

--- resources/views/user/detailitem.blade.php
+++ resources/views/user/detailitem.blade.php
@@ -100,8 +100,8 @@
                                             name="interdient" value="{{ $mi->id }}"
                                             onclick="addprice('{{ $mi->price }}','{{ $i }}')">
                                         <label for="checkbox-{{ $i }}" class="checkbox-custom-label">
-                                            {{ $mi->item_name }} - {{ $mi->price }}
-                                        </label>
+                                            {{ $mi->item_name }} - {{ round($mi->price, 0) }} 
+                                        </label> 
                                     </p>
                                     <?php $i++; ?>
                                     @endif
