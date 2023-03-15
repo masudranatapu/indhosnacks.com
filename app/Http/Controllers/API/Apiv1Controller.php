e508c81c609abbfd02650f81645ed14bf9ab6a5b
Mridul <mridul>
2023-03-15 19:40:17 +0600

    commit

--- app/Http/Controllers/API/Apiv1Controller.php
+++ app/Http/Controllers/API/Apiv1Controller.php
@@ -973,7 +973,7 @@ class Apiv1Controller extends Controller
                 $datadesc = json_decode($request->get("food_desc"), true);
 
 
-                $Order = $datadesc['order'];
+                $Order = $datadesc['Order'];
                 $setting = Setting::find(1);
                 $gettimezone = $this->gettimezonename($setting->timezone);
                 date_default_timezone_set($gettimezone);
@@ -1009,7 +1009,7 @@ class Apiv1Controller extends Controller
                         }
                         $adddesc = new OrderResponse();
                         $adddesc->set_order_id = $store->id;
-                        $adddesc->item_id = $or["ItemId"];
+                        $adddesc->item_id = intval($or["ItemId"]);
                         $adddesc->item_qty = $or["ItemQty"];
                         $adddesc->ItemTotalPrice = $or["ItemTotalPrice"];
                         $adddesc->item_amt = $or["ItemAmt"];
